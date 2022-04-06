b863924a6ac41c4f48cef997a8934c030e0a22af
vlad.andriyashen <steam.asa@gmail.com>
2022-04-06 16:55:01 +0300

    Добавлены свойства сообщений Изменен метод прослушивания

--- REST_API/IngoingConnectionPoint.cs
+++ REST_API/IngoingConnectionPoint.cs
@@ -11,7 +11,7 @@ namespace REST_API
     {
         public IMessageFactory MessageFactory { get; }
         public ESB_ConnectionPoints.PluginsInterfaces.ILogger Logger { get; }
-        Server Server;
+        Server Server = new Server();
 
         public IngoingConnectionPoint(IServiceLocator serviceLocator)
         {
@@ -29,40 +29,7 @@ namespace REST_API
         public void Run(IMessageHandler messageHandler, CancellationToken ct)
         {
             string adress = @"http://+:3367/api/";
-            HttpListener listener = new HttpListener();
-            listener.Prefixes.Add(adress);
-            listener.Start();
-            Logger.Debug($"Сервис запущен по адресу {adress}");
-
-            while (!ct.IsCancellationRequested)
-            {
-                HttpListenerContext listenerContext = listener.GetContext();
-                var Method = listenerContext.Request.HttpMethod;
-                var Path = listenerContext.Request.RawUrl;
-                string requestBody;
-                var a = listenerContext.Request.InputStream;
-                using (StreamReader sr = new StreamReader(a, Encoding.UTF8))
-                {
-                    requestBody = sr.ReadToEnd();
-                }
-                Logger.Debug(requestBody);
-                if (!string.IsNullOrEmpty(requestBody))
-                {
-                    Message message = new Message
-                    {
-                        Body = Encoding.UTF8.GetBytes(requestBody),
-                        Id = Guid.NewGuid(),
-                        Type = "DTP"
-                    };
-                    if (messageHandler.HandleMessage(message))
-                    {
-                        listenerContext.Response.StatusCode = 200;
-                        listenerContext.Response.Close();
-                    }
-                }           
-            }
-            if(ct.IsCancellationRequested)
-                listener.Stop();
+            Server.Start(adress, ct, messageHandler);
         }
         public void Cleanup()
         {
@@ -76,27 +43,46 @@ namespace REST_API
     class Server
     {
         private int count = 0;
-        public async void Start(string listenerPrefix)
+        public async void Start(string listenerPrefix , CancellationToken ct, IMessageHandler messageHandler)
         {
             HttpListener listener = new HttpListener();
             listener.Prefixes.Add(listenerPrefix);
             listener.Start();
 
-            while (true)
+            while (!ct.IsCancellationRequested)
             {
                 HttpListenerContext listenerContext = await listener.GetContextAsync();
-                var Method = listenerContext.Request.HttpMethod;
-                var Path = listenerContext.Request.RawUrl;
                 string requestBody;
-                var a = listenerContext.Request.InputStream;
-                using (StreamReader sr = new StreamReader(a, Encoding.UTF8))
+                var requestStream = listenerContext.Request.InputStream;
+                using (StreamReader sr = new StreamReader(requestStream, Encoding.UTF8))
                 {
                     requestBody = sr.ReadToEnd();
                 }
 
-                listenerContext.Response.StatusCode = 200;
-                listenerContext.Response.Close();
+                Message message = new Message();
+                message.Body = Encoding.UTF8.GetBytes(requestBody);
+                message.Id = Guid.NewGuid();
+                message.AddPropertyWithValue("PATH", listenerContext.Request.RawUrl);
+                message.AddPropertyWithValue("METHOD", listenerContext.Request.HttpMethod);
+
+                if (!string.IsNullOrEmpty(requestBody))
+                    message.Type = "DTP";
+                else
+                    message.Type = "Service";
+
+                if (messageHandler.HandleMessage(message))
+                {
+                    listenerContext.Response.StatusCode = 200;
+                    listenerContext.Response.Close();
+                }
+                else
+                {
+                    listenerContext.Response.StatusCode = 500;
+                    listenerContext.Response.Close();
+                }    
             }
+            if (ct.IsCancellationRequested)
+                listener.Stop();
         }
         private async void ProcessRequest(HttpListenerContext listenerContext)
         {
