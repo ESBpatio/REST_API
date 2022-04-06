fbeadd3382217e16240f6cb03177c16eea226590
vlad.andriyashen <steam.asa@gmail.com>
2022-04-06 16:30:46 +0300

    Добавьте файлы проекта.

---
+++ REST_API/IngoingConnectionPoint.cs
@@ -0,0 +1,106 @@
+﻿using ESB_ConnectionPoints.PluginsInterfaces;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace REST_API
+{
+    class IngoingConnectionPoint : IStandartIngoingConnectionPoint
+    {
+        public IMessageFactory MessageFactory { get; }
+        public ESB_ConnectionPoints.PluginsInterfaces.ILogger Logger { get; }
+        Server Server;
+
+        public IngoingConnectionPoint(IServiceLocator serviceLocator)
+        {
+            if (serviceLocator != null)
+            {
+                this.Logger = serviceLocator.GetLogger(this.GetType());
+                this.MessageFactory = serviceLocator.GetMessageFactory();
+            }
+        }
+        public void Initialize()
+        {
+
+        }
+
+        public void Run(IMessageHandler messageHandler, CancellationToken ct)
+        {
+            string adress = @"http://+:3367/api/";
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add(adress);
+            listener.Start();
+            Logger.Debug($"Сервис запущен по адресу {adress}");
+
+            while (!ct.IsCancellationRequested)
+            {
+                HttpListenerContext listenerContext = listener.GetContext();
+                var Method = listenerContext.Request.HttpMethod;
+                var Path = listenerContext.Request.RawUrl;
+                string requestBody;
+                var a = listenerContext.Request.InputStream;
+                using (StreamReader sr = new StreamReader(a, Encoding.UTF8))
+                {
+                    requestBody = sr.ReadToEnd();
+                }
+                Logger.Debug(requestBody);
+                if (!string.IsNullOrEmpty(requestBody))
+                {
+                    Message message = new Message
+                    {
+                        Body = Encoding.UTF8.GetBytes(requestBody),
+                        Id = Guid.NewGuid(),
+                        Type = "DTP"
+                    };
+                    if (messageHandler.HandleMessage(message))
+                    {
+                        listenerContext.Response.StatusCode = 200;
+                        listenerContext.Response.Close();
+                    }
+                }           
+            }
+            if(ct.IsCancellationRequested)
+                listener.Stop();
+        }
+        public void Cleanup()
+        {
+
+        }
+        public void Dispose()
+        {
+
+        }
+    }
+    class Server
+    {
+        private int count = 0;
+        public async void Start(string listenerPrefix)
+        {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add(listenerPrefix);
+            listener.Start();
+
+            while (true)
+            {
+                HttpListenerContext listenerContext = await listener.GetContextAsync();
+                var Method = listenerContext.Request.HttpMethod;
+                var Path = listenerContext.Request.RawUrl;
+                string requestBody;
+                var a = listenerContext.Request.InputStream;
+                using (StreamReader sr = new StreamReader(a, Encoding.UTF8))
+                {
+                    requestBody = sr.ReadToEnd();
+                }
+
+                listenerContext.Response.StatusCode = 200;
+                listenerContext.Response.Close();
+            }
+        }
+        private async void ProcessRequest(HttpListenerContext listenerContext)
+        {
+
+        }
+    }
+}
---
+++ REST_API/IngoingConnectionPointFactory.cs
@@ -0,0 +1,18 @@
+﻿using ESB_ConnectionPoints.PluginsInterfaces;
+using ESB_ConnectionPoints.Utils;
+using System.Collections.Generic;
+
+namespace REST_API
+{
+    public sealed class IngoingConnectionPointFactory : IIngoingConnectionPointFactory
+    {
+        public IIngoingConnectionPoint Create(
+            Dictionary<string, string> parameters,
+            IServiceLocator serviceLocator)
+        {
+            return (IIngoingConnectionPoint)new IngoingConnectionPoint( serviceLocator);
+            //parameters.GetStringParameter("Настройки в формате JSON"),
+            //    parameters.GetBoolParameter("Режим отладки", false),
+        }
+    }
+}
